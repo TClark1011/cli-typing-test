04a2c3c206ab80883c5f1100f14197cc5a59455f
Thomas Clark <thomas.clark@bigtincan.com>
2022-05-20 10:04:01 +1000

    setup react + ink

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Text } from "ink";
+
+const App: React.FC = () => {
+	return <Text>Hello (with react!)</Text>;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { render } from "ink";
+import { StrictMode } from "react";
+import App from "./App";
+
+render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
