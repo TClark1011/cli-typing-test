da8b11ebb6c6cb11feaac327def816bd61fca49e
Thomas Clark <thomas.clark@bigtincan.com>
2022-05-24 13:03:37 +1000

    finalised help message

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,6 +1,10 @@
 import * as z from "zod";
+import PACKAGE_JSON from "../../package.json";
 
 export const DEFAULT_NUMBER_OF_WORDS = z
 	.preprocess(Number, z.number())
 	.parse(process.env.DEFAULT_NUMBER_OF_WORDS);
-// export const DEFAULT_NUMBER_OF_WORDS = 15;
+
+export const APP_RUN_COMMAND = Object.keys(PACKAGE_JSON.bin)[0];
+export const APP_NAME = PACKAGE_JSON.name;
+export const APP_DESCRIPTION = PACKAGE_JSON.description;
--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -1,18 +1,38 @@
-import { DEFAULT_NUMBER_OF_WORDS } from "$constants";
+import {
+	DEFAULT_NUMBER_OF_WORDS,
+	APP_RUN_COMMAND,
+	APP_NAME,
+	APP_DESCRIPTION,
+} from "$constants";
 import cliHelper from "$cli-helper";
 import chalk from "chalk";
+import { A, flow, pipe, S } from "@mobily/ts-belt";
+import gradient from "gradient-string";
 
-const paramText = chalk.blueBright;
+const paramText = chalk.cyan;
 const header = chalk.bold;
+const defaultValue = chalk.gray;
+
+const spaceOutCharacters = flow(S.split(""), A.join(" "));
+const styleTitle = flow(gradient.pastel, chalk.bold);
+
+const TITLE = pipe(APP_NAME, S.toUpperCase, spaceOutCharacters, styleTitle);
 
 const cli = cliHelper(
-	`
- ${header("Usage")}
+	`${APP_DESCRIPTION}
 
- ${paramText("-w --words")}      number of words in the test (default: 60)
- ${paramText(
-		"-m --maxLength",
- )}  maximum length of a word, use 0 for no limit (default: 0)
+${header("Usage")}
+
+${paramText(APP_RUN_COMMAND)}          start typing test
+
+${header("Parameters")}
+
+${paramText("-w --words")}      number of words in the test ${defaultValue(
+		"(default: 60)",
+	)}
+${paramText(
+	"-m --maxLength",
+)}  maximum length of a word, use 0 for no limit ${defaultValue("default: 0")}
 `,
 	{
 		flags: {
@@ -27,6 +47,7 @@ const cli = cliHelper(
 				alias: "m",
 			},
 		},
+		description: TITLE,
 	},
 );
 
