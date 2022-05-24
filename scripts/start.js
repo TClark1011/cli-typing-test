a82bd1412a3f1b8cf814a11a640d88b1d1aafaa7
Thomas Clark <thomas.clark@bigtincan.com>
2022-05-24 14:44:56 +1000

    improved build process
    tsconfig paths are now replaced during build process

--- scripts/start.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-require("./ts-config-paths-bootstrap");
-require("../build/index.js")
--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,9 +1,10 @@
+import { F, flow, R } from "@mobily/ts-belt";
 import * as z from "zod";
 import PACKAGE_JSON from "../../package.json";
 
 export const DEFAULT_NUMBER_OF_WORDS = z
 	.preprocess(Number, z.number())
-	.parse(process.env.DEFAULT_NUMBER_OF_WORDS);
+	.parse(process.env.DEFAULT_NUMBER_OF_WORDS ?? "60");
 
 export const APP_RUN_COMMAND = Object.keys(PACKAGE_JSON.bin)[0];
 export const APP_NAME = PACKAGE_JSON.name;
